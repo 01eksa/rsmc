139ac85721f27575dae08d7eefc03ea3b1e19d7f
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-07-11 23:13:16 +0300

    feat(types.h): fix enum sizes and struct layout for stable P/Invoke ABI

--- include/rsmc/types.h
+++ include/rsmc/types.h
@@ -6,45 +6,66 @@
 extern "C" {
 #endif
 
-// types
+// enums
 
-enum RsmcBoardLimits {
+enum {
     RsmcBoardSize = 8,
     RsmcMinCoord = 0,
-    RsmcMaxCoord = RsmcBoardSize - 1
+    RsmcMaxCoord = RsmcBoardSize - 1,
+    RsmcMaxValidMoves = 34, // theoretical max count of valid moves
 };
 
-typedef enum {
-    RsmcPlayerWhite,
-    RsmcPlayerBlack
-} RsmcPlayer;
+typedef uint8_t RsmcPlayer;
+enum {
+    RsmcPlayerWhite = 0,
+    RsmcPlayerBlack = 1
+};
 
-typedef enum {
-    RsmcGameStatusContinue,
-    RsmcGameStatusDraw,
-    RsmcGameStatusWhiteWin,
-    RsmcGameStatusBlackWin
-} RsmcGameStatus;
+typedef uint8_t RsmcGameStatus;
+enum {
+    RsmcGameStatusContinue = 0,
+    RsmcGameStatusDraw = 1,
+    RsmcGameStatusWhiteWin = 2,
+    RsmcGameStatusBlackWin = 3
+};
 
-typedef enum {
-    RsmcBoardCellEmpty,
-    RsmcBoardCellWhite,
-    RsmcBoardCellBlack
-} RsmcBoardCell;
+typedef uint8_t RsmcBoardCell;
+enum {
+    RsmcBoardCellEmpty = 0,
+    RsmcBoardCellWhite = 1,
+    RsmcBoardCellBlack = 2
+};
+
+// structs
+
+typedef struct {
+    uint8_t white_score;
+    uint8_t black_score;
+} RsmcPlayersScore;
+_Static_assert(sizeof(RsmcPlayersScore) == 2, "unexpected padding in RsmcPlayersScore");
+
+typedef struct {
+    RsmcPlayersScore score;
+    RsmcGameStatus game_status;
+} RsmcGameState;
+_Static_assert(sizeof(RsmcGameState) == 3, "unexpected padding in RsmcGameState");
 
 typedef struct {
     int8_t x;
     int8_t y;
 } RsmcCoords;
+_Static_assert(sizeof(RsmcCoords) == 2, "unexpected padding in RsmcCoords");
 
 typedef struct {
     RsmcBoardCell cells[RsmcBoardSize][RsmcBoardSize];
 } RsmcBoard;
+_Static_assert(sizeof(RsmcBoard) == 64, "unexpected padding in RsmcBoard");
 
 typedef struct {
     uint8_t count;
-    RsmcCoords coords[34];
+    RsmcCoords coords[RsmcMaxValidMoves];
 } RsmcMoves;
+_Static_assert(sizeof(RsmcMoves) == 69, "unexpected padding in RsmcMoves");
 
 // type extensions
 
