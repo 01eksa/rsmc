5eee9d60a24c564bf9abf3de61e30f258e60c7ba
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-07-11 23:16:49 +0300

    feat(rsmc.h): add rsmc API

--- include/rsmc.h
+++ include/rsmc.h
@@ -2,9 +2,25 @@
 #define RSMC_RSMC_H
 #include "rsmc/types.h"
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+void rsmc_set_start_position(RsmcBoard *board);
+
+bool rsmc_apply_move(RsmcBoard *board, RsmcCoords coords, RsmcPlayer player);
+
+uint8_t rsmc_get_player_score(const RsmcBoard *board, RsmcPlayer player);
+
+RsmcPlayersScore rsmc_get_players_score(const RsmcBoard *board);
+
+RsmcMoves rsmc_get_valid_moves(const RsmcBoard *board, RsmcPlayer player);
+
+RsmcGameStatus rsmc_get_game_status(const RsmcBoard *board);
+
+RsmcGameState rsmc_get_game_state(const RsmcBoard *board);
 
 #ifdef __cplusplus
 }
