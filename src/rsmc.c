1faac58b829a72441a26186a3d013ddcf6a387d6
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-07-12 18:19:45 +0300

    feat(rsmc.c): implement rsmc_get_valid_moves

--- src/rsmc.c
+++ src/rsmc.c
@@ -136,3 +136,20 @@ bool rsmc_is_move_valid(const RsmcBoard *board, const RsmcCoords coords, const R
 
     return false;
 }
+
+RsmcMoves rsmc_get_valid_moves(const RsmcBoard *board, RsmcPlayer player)
+{
+    RsmcMoves result = {0};
+
+    const int8_t board_size = RsmcBoardSize;
+    for (int8_t y = 0; y < board_size; y++) {
+        for (int8_t x = 0; x < board_size; x++) {
+            const RsmcCoords coords = {x, y};
+            if (rsmc_is_move_valid(board, coords, player)) {
+                result.coords[result.count++] = coords;
+            }
+        }
+    }
+
+    return result;
+}
