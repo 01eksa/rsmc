30da1b61e1004e95ede37fb338a434af8069b88e
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-07-13 00:44:06 +0300

    docs(rsmc.h): cover API with docs

--- include/rsmc.h
+++ include/rsmc.h
@@ -1,3 +1,9 @@
+/**
+ * @file rsmc.h
+ * @brief reversi stateless model in C
+ *
+ */
+
 #ifndef RSMC_RSMC_H
 #define RSMC_RSMC_H
 #include "rsmc/types.h"
@@ -8,20 +14,65 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Sets start position on the board.
+ *
+ * @param board a pointer to RsmcBoard, which will be changed.
+ */
 void rsmc_set_start_position(RsmcBoard *board);
 
+/**
+ * @brief Set piece and flip opponent's pieces if move is valid.
+ *
+ * @param board a pointer to RsmcBoard, which will be changed if success.
+ * @param coords RsmcCoords of cell where you want to place a piece.
+ * @param player RsmcPlayer to make a move for.
+ * @return true if success, false if move is impossible or coords / player are invalid.
+ */
 bool rsmc_apply_move(RsmcBoard *board, RsmcCoords coords, RsmcPlayer player);
 
 uint8_t rsmc_get_player_score(const RsmcBoard *board, RsmcPlayer player);
 
+/**
+ * @brief Calculates score of both players in one pass.
+ *
+ * @param board a constant pointer to RsmcBoard.
+ * @return RsmcPlayersScore which contains score for both players.
+ */
 RsmcPlayersScore rsmc_get_players_score(const RsmcBoard *board);
 
+/**
+ * @brief Checks if move is valid.
+ *
+ * @param board a constant pointer to RsmcBoard.
+ * @param coords RsmcCoords of cell where you want to place a piece.
+ * @param player RsmcPlayer to check a move for.
+ * @return true if move is valid, false if not (or coords / player is invalid)
+ */
 bool rsmc_is_move_valid(const RsmcBoard *board, RsmcCoords coords, RsmcPlayer player);
 
+/**
+ * @brief Finds all valid moves.
+ *
+ * @param board a constant pointer to RsmcBoard.
+ * @param player RsmcPlayer to search valid moves for.
+ * @return RsmcMoves, where counts will be 0 if there are no legal moves or player is invalid.
+ */
 RsmcMoves rsmc_get_valid_moves(const RsmcBoard *board, RsmcPlayer player);
 
+/**
+ * @brief Extracts game status. Wrapper around rsmc_get_game_state.
+ *
+ * @param board a constant pointer to RsmcBoard.
+ * @return Current RsmcGameStatus.
+ */
 RsmcGameStatus rsmc_get_game_status(const RsmcBoard *board);
 
+/** @brief Analyzes current game state: finds player score and game status.
+ *
+ * @param board a constant pointer to RsmcBoard.
+ * @return current RsmcGameState.
+ */
 RsmcGameState rsmc_get_game_state(const RsmcBoard *board);
 
 #ifdef __cplusplus
